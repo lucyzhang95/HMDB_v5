f29689c2d99c1d86e3756b0816045c08e96aad34
Lucy Zhang <bailin.zhang1116@gmail.com>
2025-05-16 17:33:38 -0700

    add strip_tag_namespace fn

---
+++ hmdb_v5_parser.py
@@ -0,0 +1,21 @@
+from lxml import etree as ET
+import os
+import zipfile
+
+CACHE_DIR = os.path.join(os.getcwd(), "cache")
+os.makedirs(CACHE_DIR, exist_ok = True)
+
+def strip_tag_namespace(tag: str) -> str:
+    """Strip namespace from main element tag
+    Each element tag from HMDB contains "http://www.hmdb.ca" as namespace
+    e.g. {http://www.hmdb.ca}accession
+    Remove the namespace for easier data processing
+
+    :param tag: element tags (e.g. <accession>HMDB0000001</accession>)
+    :return: original tag without namespace
+    """
+    idx = tag.rfind("}")
+    # rfind() method "not found" == -1
+    if idx != -1:  # if idx is not "not found"
+        tag = tag[idx + 1 :]
+    return tag

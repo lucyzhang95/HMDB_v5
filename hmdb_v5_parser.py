e72e2843884c9f3aa42334c72a13a1c2202d7a00
Lucy Zhang <bailin.zhang1116@gmail.com>
2025-05-21 23:16:30 -0700

    add entrez query fn

--- hmdb_v5_parser.py
+++ hmdb_v5_parser.py
@@ -1,10 +1,12 @@
 import os
 import pathlib
 import ssl
+import time
 import zipfile
 from typing import Iterator
 
 import requests
+from Bio import Entrez
 from ete3 import NCBITaxa
 from lxml import etree as ET
 
@@ -58,10 +60,11 @@ def get_all_microbe_names(input_xml: str | pathlib.Path) -> Iterator[str]:
 
 def ete3_taxon_name2taxid(taxon_names: list) -> dict:
     """Use ete3 to map taxonomy names to NCBI taxonomy ids
-    ete3 is good at mapping exact taxonomy names and fast without accessing API
+    ete3 is good at mapping exact taxonomy names and fast without accessing the API
 
     :param taxon_names: a list of taxon names
     :return: a dictionary mapping taxon names to taxids
+    e.g., {'veillonella alcalescens': {'taxid': 29466, 'mapping_tool': 'ete3'}, ...}
     """
     ete3_mapped = {}
     taxon_names = set(taxon_names)
@@ -77,6 +80,33 @@ def ete3_taxon_name2taxid(taxon_names: list) -> dict:
         if taxid:
             ete3_mapped[name] = {"taxid": int(taxid[0]), "mapping_tool": "ete3"}
     return ete3_mapped
+
+
+def entrez_taxon_name2taxid(taxon_names: list[str], sleep=0.34, email="bazhang@scripps.edu") -> dict:
+    """Map taxonomy names to NCBI taxonomy ids using entrez API
+    Entrez is good at mapping recent reclassified taxonomy names that are outdated
+    but very slow due to no batch query allowed, so expensive to query, recommend cache the output
+
+    :param email:
+    :param taxon_names:
+    :return: a dictionary mapping taxon names to taxids,
+    e.g., {'cellulomonas galba': {'taxid': 401861, 'mapping_tool': 'entrez'}, ...}
+    """
+    Entrez.email = email
+    entrez_mapped = {}
+
+    for name in set(taxon_names):
+        try:
+            handle = Entrez.esearch(db="taxonomy", term=name, retmode="xml", retmax=1)
+            record = Entrez.read(handle)
+            handle.close()
+            if record["IdList"]:
+                taxid = int(record["IdList"][0])
+                entrez_mapped[name] = {"taxid": taxid, "mapping_tool": "entrez"}
+        except Exception as e:
+            print(f"Entrez query failed for '{name}': {e}")
+        time.sleep(sleep)
+    return entrez_mapped
 
 
 def get_ncit_taxon_description(taxon_names):

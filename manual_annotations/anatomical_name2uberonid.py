38a8ca784dffe5f26f9307d9c4d76efb02571d03
Lucy Zhang <bailin.zhang1116@gmail.com>
2025-08-27 00:19:01 -0700

    add updated version of uberon mapped entities

--- manual_annotations/anatomical_name2uberonid.py
+++ manual_annotations/anatomical_name2uberonid.py
@@ -16,13 +16,59 @@ OXO_CORRECTIONS = {
 }
 
 
-def get_oxo_correction():
+MANUAL_ANATOMY_MAPPINGS = {
+    "neuron": {
+        "id": "CL:0000540",
+        "name": "neuron",
+        "original_name": "neuron",
+        "type": "biolink:Cell",
+    },
+    "erythrocyte": {
+        "id": "CL:0000232",
+        "name": "erythrocyte",
+        "original_name": "erythrocyte",
+        "type": "biolink:Cell",
+    },
+    "leukocyte": {
+        "id": "CL:0000738",
+        "name": "leukocyte",
+        "original_name": "leukocyte",
+        "type": "biolink:Cell",
+    },
+    "platelet": {
+        "id": "CL:0000233",
+        "name": "platelet",
+        "original_name": "platelet",
+        "type": "biolink:Cell",
+    },
+    "cellular cytoplasm": {
+        "id": "GO:0005737",
+        "name": "cytoplasm",
+        "original_name": "cellular cytoplasm",
+        "type": "biolink:CellularComponent",
+    },
+    "pericardial effusion": {
+        "id": "UBERON:0002406",
+        "name": "pericardial sac",
+        "original_name": "pericardial effusion",
+        "type": "biolink:AnatomicalEntity",
+    },
+}
+
+
+def _get_uberon_correction():
     return OXO_CORRECTIONS.copy()
 
 
-def apply_oxo_correction(uberon_mapped: dict) -> dict:
-    correction = get_oxo_correction()
+def _apply_uberon_corrections(uberon_mapped: dict) -> dict:
+    correction = _get_uberon_correction()
     for name, corrected_data in correction.items():
         if name in uberon_mapped:
             uberon_mapped[name] = corrected_data
     return uberon_mapped
+
+
+def apply_manual_anatomy_mappings(uberon_mapped: dict) -> dict:
+    uberon_corrected = _apply_uberon_corrections(uberon_mapped)
+    uberon_corrected.update(MANUAL_ANATOMY_MAPPINGS)
+    return uberon_corrected
